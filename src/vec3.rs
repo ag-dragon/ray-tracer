94b30f84323edc73ba8c55ee8ebb3cdda7d59711
ag-dragon <h28941+github@gmail.com>
2023-07-09 23:49:04 -0400

    Implement vector operator overloads

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,6 +1,12 @@
-use std::ops::{{Add}};
+use std::ops::{{
+    Add, AddAssign,
+    Div, DivAssign,
+    Index, IndexMut,
+    Mul, MulAssign, Neg,
+    Sub, SubAssign,
+}};
 
-#[derive(Debug)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Vec3<T> {
     pub x: T,
     pub y: T,
@@ -13,15 +19,130 @@ impl<T> Vec3<T> {
     }
 }
 
-impl<T: Add<Output = T>> Add for Vec3<T> {
+impl<T> Add for Vec3<T> where
+    T: Add<Output = T> {
     type Output = Self;
 
     #[inline(always)]
-    fn add(self, other: Self) -> Self {
+    fn add(self, rhs: Self) -> Self::Output {
         Self {
-            x: self.x + other.x,
-            y: self.y + other.y,
-            z: self.z + other.z,
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl<T> AddAssign for Vec3<T> where 
+    T: Copy + Add<Output = T> {
+    #[inline(always)]
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl<T> Sub for Vec3<T> where
+    T: Sub<Output = T> {
+    type Output = Self;
+
+    #[inline(always)]
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl<T> SubAssign for Vec3<T> where
+    T: Copy + Sub<Output = T> {
+    #[inline(always)]
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+impl<T> Mul for Vec3<T> where
+    T: Mul<Output = T> {
+    type Output = Self;
+
+    #[inline(always)]
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        }
+    }
+}
+
+impl<T> MulAssign for Vec3<T> where
+    T: Copy + Mul<Output = T> {
+    #[inline(always)]
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
+    }
+}
+
+impl<T> Neg for Vec3<T> where
+    T: Neg<Output = T> {
+    type Output = Self;
+
+    #[inline(always)]
+    fn neg(self) -> Self::Output {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl<T> Div for Vec3<T> where
+    T: Div<Output = T> {
+    type Output = Self;
+
+    #[inline(always)]
+    fn div(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+            z: self.z / rhs.z,
+        }
+    }
+}
+
+impl<T> DivAssign for Vec3<T> where
+    T: Copy + Div<Output = T> {
+    #[inline(always)]
+    fn div_assign(&mut self, rhs: Self) {
+        *self = *self / rhs;
+    }
+}
+
+impl<T> Index<usize> for Vec3<T> {
+    type Output = T;
+
+    #[inline(always)]
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            i => panic!("index {i} out of bounds"),
+        }
+    }
+}
+
+impl<T> IndexMut<usize> for Vec3<T> {
+    #[inline(always)]
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        match index {
+            0 => &mut self.x,
+            1 => &mut self.y,
+            2 => &mut self.z,
+            i => panic!("index {i} out of bounds"),
         }
     }
 }

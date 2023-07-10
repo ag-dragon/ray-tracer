90bbb6c3b2f919c2c13d96a2968e38e355845fb6
ag-dragon <h28941+github@gmail.com>
2023-07-10 18:24:32 -0400

    Switch to using Color type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod vec3;
 use vec3::Vec3;
 
+type Color = Vec3<i32>;
+
 fn main() {
     let image_width = 256;
     let image_height = 256;
@@ -11,19 +13,18 @@ fn main() {
 
     for j in (0..image_height).rev() {
         for i in 0..image_width {
-            let r = (i as f64) / ((image_width as f64) - 1.0);
-            let g = (j as f64) / ((image_height as f64) - 1.0);
-            let b = 0.25;
+            let pixel_color = Color::new(
+                (((i as f64) / ((image_width as f64) - 1.0)) * 255.999) as i32,
+                (((j as f64) / ((image_height as f64) - 1.0)) * 255.999) as i32,
+                (0.25 * 255.999) as i32
+            );
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            println!("{ir} {ig} {ib}");
+            println!(
+                "{} {} {}",
+                pixel_color.x,
+                pixel_color.y,
+                pixel_color.z
+            );
         }
     }
-
-    let v = Vec3::new(1.0, 1.0, 1.0);
-    
-    eprintln!("{:?}", v);
 }

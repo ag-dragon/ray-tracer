33f7328749fd023e894071e96f032ed5c022a67e
ag-dragon <h28941+github@gmail.com>
2023-07-10 21:57:40 -0400

    Add normal shading

--- src/ray.rs
+++ src/ray.rs
@@ -21,14 +21,22 @@ impl<T: Float> Ray<T> {
     pub fn color(&self) -> Color {
         let mut c = Vec3::<T>::zero();
         c.z = T::from(-1.0).unwrap();
-        if self.hit_sphere(c, 0.5) {
-            return Color::new(255, 0, 0)
+        let mut t = self.hit_sphere(c, 0.5);
+        if t > 0.0 {
+            let mut n = (self.at(T::from(t).unwrap()) - c).normalized();
+            n += Vec3::<T>::one();
+            n *= T::from(0.5).unwrap();
+            return Color::new(
+                num::cast(n.x * T::from(255.999).unwrap()).unwrap(),
+                num::cast(n.y * T::from(255.999).unwrap()).unwrap(),
+                num::cast(n.z * T::from(255.999).unwrap()).unwrap()
+                )
         }
         let unit_direction = self.direction.normalized();
-        let t = (unit_direction.y + T::from(1.0).unwrap()) * T::from(0.5).unwrap();
+        t = num::cast((unit_direction.y + T::from(1.0).unwrap()) * T::from(0.5).unwrap()).unwrap();
 
-        let res = Vec3::<T>::one()*(T::from(1.0).unwrap() - t)
-            + Vec3::new(T::from(0.5).unwrap(), T::from(0.7).unwrap(), T::from(1.0).unwrap())*t;
+        let res = Vec3::<T>::one()*(T::from(1.0).unwrap() - T::from(t).unwrap())
+            + Vec3::new(T::from(0.5).unwrap(), T::from(0.7).unwrap(), T::from(1.0).unwrap())*T::from(t).unwrap();
         Color::new(
             num::cast(res.x * T::from(255.999).unwrap()).unwrap(),
             num::cast(res.y * T::from(255.999).unwrap()).unwrap(),
@@ -36,13 +44,15 @@ impl<T: Float> Ray<T> {
         )
     }
 
-    fn hit_sphere(&self, center: Vec3<T>, radius: f64) -> bool {
+    fn hit_sphere(&self, center: Vec3<T>, radius: f64) -> f64 {
         let oc = self.origin - center;
         let a = self.direction.dot(self.direction);
         let b = oc.dot(self.direction) * T::from(2.0).unwrap();
         let c = oc.dot(oc) - T::from(radius*radius).unwrap();
         let discriminant = b*b - T::from(4.0).unwrap()*a*c;
-        discriminant > T::from(0.0).unwrap()
+        if (discriminant < T::from(0.0).unwrap()) { -1.0 } else {
+            num::cast((-b - discriminant.sqrt()) / (a*T::from(2.0).unwrap())).unwrap()
+        }
     }
 }
 

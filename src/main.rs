1ddcd74ae5b972557c737a0ba82ba04cbc4551a2
ag-dragon <h28941+github@gmail.com>
2023-07-10 21:36:47 -0400

    Get sphere collision

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 mod vectors;
 mod ray;
+
+use num::traits::{Float};
+
 use vectors::Vec3;
+use vectors::Color;
 use ray::Ray;
 
-type Color = Vec3<i32>;
-
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = ((image_width as f64) / aspect_ratio) as i32;
+    eprintln!("{image_width} {image_height} {aspect_ratio}");
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin: Vec3<f64> = Vec3::zero();
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - (horizontal/2.0)
+        - (vertical/2.0) - Vec3::new(0.0, 0.0, focal_length);
 
     println!("P3");
     println!("{image_width} {image_height}");
@@ -15,12 +31,14 @@ fn main() {
 
     for j in (0..image_height).rev() {
         for i in 0..image_width {
-            let pixel_color = Color::new(
-                (((i as f64) / ((image_width as f64) - 1.0)) * 255.999) as i32,
-                (((j as f64) / ((image_height as f64) - 1.0)) * 255.999) as i32,
-                (0.25 * 255.999) as i32
-            );
+            let u = (i as f64) / ((image_width as f64) - 1.0);
+            let v = (j as f64) / ((image_height as f64) - 1.0);
+            let r = Ray::new(
+                origin,
+                lower_left_corner + horizontal*u + vertical*v - origin
+                );
 
+            let pixel_color = r.color();
             println!(
                 "{} {} {}",
                 pixel_color.x,
--- src/ray.rs
+++ src/ray.rs
@@ -1,21 +1,48 @@
+use num::traits::{Float, int};
+
 use crate::vectors::Vec3;
+use crate::vectors::Color;
 
-use std::ops::{{Add, Mul}};
-
-#[derive(Copy, Clone, PartialEq, Debug)]
-pub struct Ray<T> {
+#[derive(Copy, Clone, Debug)]
+pub struct Ray<T: Float> {
     pub origin: Vec3<T>,
     pub direction: Vec3<T>,
 }
 
-impl<T> Ray<T> where 
-    T: Copy + Mul<Output = T> + Add<Output = T> { 
+impl<T: Float> Ray<T> {
     pub fn new(origin: Vec3<T>, direction: Vec3<T>) -> Self {
         Self { origin, direction }
     }
 
     pub fn at(&self, t: T) -> Vec3<T> {
         self.origin + (self.direction*t)
+    }
+
+    pub fn color(&self) -> Color {
+        let mut c = Vec3::<T>::zero();
+        c.z = T::from(-1.0).unwrap();
+        if self.hit_sphere(c, 0.5) {
+            return Color::new(255, 0, 0)
+        }
+        let unit_direction = self.direction.normalized();
+        let t = (unit_direction.y + T::from(1.0).unwrap()) * T::from(0.5).unwrap();
+
+        let res = Vec3::<T>::one()*(T::from(1.0).unwrap() - t)
+            + Vec3::new(T::from(0.5).unwrap(), T::from(0.7).unwrap(), T::from(1.0).unwrap())*t;
+        Color::new(
+            num::cast(res.x * T::from(255.999).unwrap()).unwrap(),
+            num::cast(res.y * T::from(255.999).unwrap()).unwrap(),
+            num::cast(res.z * T::from(255.999).unwrap()).unwrap()
+        )
+    }
+
+    fn hit_sphere(&self, center: Vec3<T>, radius: f64) -> bool {
+        let oc = self.origin - center;
+        let a = self.direction.dot(self.direction);
+        let b = oc.dot(self.direction) * T::from(2.0).unwrap();
+        let c = oc.dot(oc) - T::from(radius*radius).unwrap();
+        let discriminant = b*b - T::from(4.0).unwrap()*a*c;
+        discriminant > T::from(0.0).unwrap()
     }
 }
 

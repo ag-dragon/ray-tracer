1713d8e06fa13950f5104ac44aa8f29f10b7d031
ag-dragon <h28941+github@gmail.com>
2023-08-09 15:57:07 -0400

    Triangles with vertex normals now work

--- src/scene/mesh_test.rs
+++ src/scene/mesh_test.rs
@@ -6,15 +6,15 @@ use crate::shape::{Hittable, Mesh, Sphere};
 use crate::Camera;
 
 pub fn gen_scene() -> Scene {
-    let lookfrom = Vec3::new(0.0, 3.0, 0.0);
-    let lookat = Vec3::new(5.0, -1.0, -8.0);
+    let lookfrom = Vec3::new(0.0, 3.0, 22.0);
+    let lookat = Vec3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
     let aspect_ratio = 3.0 / 2.0;
     let cam = Camera::new(
         lookfrom,
         lookat,
         vup,
-        40.0,
+        70.0,
         aspect_ratio,
         0.0,
         5.0
@@ -28,13 +28,8 @@ pub fn gen_scene() -> Scene {
     let test_material = Dielectric {
         ir: 1.5
     };
-    objects.push(Box::new(Mesh::load(String::from("./examples/duck.obj"),
-        test_material
-    )));
-    objects.push(Box::new(Sphere::new(
-                Vec3::new(5.0, -1.0, -8.0),
-                1.0,
-                Lambertian { albedo: SolidColor { color: Color::new(0.2, 0.2, 0.2) } }
+    objects.push(Box::new(Mesh::load(String::from("./examples/teapot2.obj"),
+        mesh_material
     )));
     let skybox = ImageTexture::load(String::from("./examples/textures/sky.png"));
     Scene::new(cam, objects, Some(skybox))
